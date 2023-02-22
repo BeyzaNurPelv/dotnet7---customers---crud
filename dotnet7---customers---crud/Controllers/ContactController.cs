876986a74f1fbfe9c7e582e9524e5365436bf28c
BeyzaNurPelv <beyzanurpelvanoglu@gmail.com>
2023-02-22 23:52:05 +0300

    Initial commit

---
+++ dotnet7---customers---crud/Controllers/ContactController.cs
@@ -0,0 +1,56 @@
+﻿using dotnet7___customers___crud.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet7___customers___crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        static public List<Contact> Contacts = new List<Contact>
+        {
+            new Contact {Id=1, Name="Bruce Wayne ", Birthdate=new DateTime (1915,4,17)},
+             new Contact {Id=2, Name= "Peter Parker ", Birthdate=new DateTime (2001,8,10)},
+
+        };
+        [HttpGet]
+        public ActionResult<List<Contact>>GetAllContacts()
+        {
+            return Ok(Contacts);
+        }
+        [HttpGet(" {id }")]
+        public ActionResult<Contact>GetContact(int id)
+        {
+            var contact =Contacts.Find(x => x.Id == id);
+            if (contact == null)
+                return NotFound("Nope.");
+            return Ok(contact);
+        }
+        [HttpPost]
+        public ActionResult<List<Contact>> CreateContact(Contact contact)
+        {
+            Contacts.Add(contact);
+            return Ok(Contacts);
+        }
+        [HttpPut]
+        public ActionResult<List<Contact>> UpdateContact(Contact updatedContact)
+        {
+            var contact=Contacts.Find(x => x.Id == updatedContact.Id);
+            if(contact == null)
+                return NotFound("Nope.");
+            contact.Name = updatedContact.Name;
+            contact.Birthdate = updatedContact.Birthdate;
+            return Ok(Contacts);
+        }
+        [HttpDelete (" {id}")]
+        public ActionResult<List<Contact>> Delete(int id )
+        {
+            var contact = Contacts.Find(x => x.Id ==id);
+            if (contact == null)
+                return NotFound("Nope.");
+            Contacts.Remove(contact);
+            return Ok(Contacts);
+        }
+    }
+}
---
+++ dotnet7---customers---crud/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace dotnet7___customers___crud.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime Birthdate { get; set; }
+
+    }
+}
